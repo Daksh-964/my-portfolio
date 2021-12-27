e58fad3a856a5fe400cf8803e6102b80de374c41
Daksh Singh <dakshsingh2608@gmail.com>
2021-12-27 19:57:38 +0530

    adding in sanity studio

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div>
+           <div className='header'>
+                    <h1>Hi this is header</h1>
+           </div>
+        </div>
+    )
+}
+
+export default Header
